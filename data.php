7123e1f6d51afc25aabf94a0b6d02044b71c7702
Natalya Romanova <Natalochka_ne@mail.ru>
2023-06-03 12:53:00 +0300

    task 3.6 completed

---
+++ data.php
@@ -0,0 +1,49 @@
+<?php
+
+$categories = [
+    "boards" => "Доски и лыжи",
+    "attachment" => "Крепления",
+    "boots" => "Ботинки",
+    "clothing" => "Одежда",
+    "tools" => "Инструменты",
+    "other" => "Разное"
+];
+
+$goods = [
+    [
+        "name" => "2014 Rossignol District Snowboard",
+        "category" => "Доски и лыжи",
+        "start_price" => 10999,
+        "image" => "img/lot-1.jpg"
+    ],
+    [
+        "name" => "DC Ply Mens 2016/2017 Snowboard",
+        "category" => "Доски и лыжи",
+        "start_price" => 159999,
+        "image" => "img/lot-2.jpg"
+    ],
+    [
+        "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
+        "category" => "Крепления",
+        "start_price" => 8000,
+        "image" => "img/lot-3.jpg"
+    ],
+    [
+        "name" => "Ботинки для сноуборда DC Mutiny Charocal",
+        "category" => "Ботинки",
+        "start_price" => 10999,
+        "image" => "img/lot-4.jpg"
+    ],
+    [
+        "name" => "Куртка для сноуборда DC Mutiny Charocal",
+        "category" => "Одежда",
+        "start_price" => 7500,
+        "image" => "img/lot-5.jpg"
+    ],
+    [
+        "name" => "Маска Oakley Canopy",
+        "category" => "Разное",
+        "start_price" => 5400,
+        "image" => "img/lot-6.jpg"
+    ]
+];
--- functions.php
+++ functions.php
@@ -1,9 +1,12 @@
 <?php
 
-$format_sum = function ($sum) {
+function format_sum($sum)
+{
     $sum = ceil($sum);
     if ($sum > 1000) {
         $sum = number_format($sum, 0, "", " ");
     }
     return $sum . " " . "₽";
-};
+}
+
+;
