ff5a20ea16884463f0329541c6422b2a4b46c5d6
Natalya Romanova <Natalochka_ne@mail.ru>
2023-06-05 21:59:38 +0300

    task 4.9 completed

---
+++ schema.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE my_yeticave;
+USE my_yeticave;
+
+CREATE TABLE user
+(
+    id                INT AUTO_INCREMENT PRIMARY KEY,
+    date_registration DATE,
+    email             VARCHAR(128) NOT NULL UNIQUE,
+    user_name         VARCHAR(128),
+    user_password     CHAR(15),
+    user_contacts     TEXT
+);
+
+CREATE TABLE categories
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    symbol_code   VARCHAR(128),
+    name_category VARCHAR(128)
+);
+
+CREATE TABLE lots
+(
+    id              INT AUTO_INCREMENT PRIMARY KEY,
+    date_create     DATETIME DEFAULT CURRENT_TIMESTAMP,
+    lot_name        VARCHAR(128),
+    lot_description VARCHAR(128),
+    lot_image       VARCHAR(128),
+    start_price     INT,
+    date_finish     DATE,
+    bet_step        INT,
+    user_id         INT,
+    winner_id       INT,
+    category_id     INT,
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (winner_id) REFERENCES user (id),
+    FOREIGN KEY (category_id) REFERENCES categories (id)
+);
+
+CREATE TABLE bets
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    bet_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    sum      INT,
+    FOREIGN KEY (id) REFERENCES user (id),
+    FOREIGN KEY (id) REFERENCES lots (id)
+)
